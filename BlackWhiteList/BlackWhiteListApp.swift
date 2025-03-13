0721a238e761ecbd886cfc105674add9a2f9ca44
Chieh-Yu Tsai <joy20396@gmail.com>
2025-03-13 09:51:19 +0800

    Initial Commit

---
+++ BlackWhiteList/BlackWhiteListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackWhiteListApp.swift
+//  BlackWhiteList
+//
+//  Created by 蔡傑宇 on 2025/3/13.
+//
+
+import SwiftUI
+
+@main
+struct BlackWhiteListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
